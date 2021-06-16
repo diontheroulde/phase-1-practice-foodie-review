820837b21fda3decffdd8c2c6c08127d6782488b
Dion Theroulde <dionjr105@gmail.com>
2021-06-16 16:13:28 -0400

    Adjusted comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,88 @@
-// Code here
+const roastPorkUrl = "http://localhost:3000/dishes/1"
+const dishName = document.querySelector(".dish-details h2")
+const dishImg = document.querySelector(".dish-details img")
+const dishDescription = document.querySelector(".description textarea")
+const customerReview = document.querySelector("ul.reviews")
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  fetch(roastPorkUrl)
+  .then(res => res.json())
+  .then(data => {
+    dishName.innerHTML = data.name
+    dishImg.src = data.image_url
+    dishDescription.innerHTML = data.description
+    const reviews = data.reviews
+    reviews.forEach(review => {
+      const dishReviews = document.createElement("li") 
+      customerReview.append(dishReviews)
+      dishReviews.innerHTML = reviews
+
+    })
+    
+    })
+      
+    })
+      
+
+  
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
