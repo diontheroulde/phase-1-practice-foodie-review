c9d739d33993bd6bec7bbacb498164f9afdff7e8
Dion Theroulde <dionjr105@gmail.com>
2021-06-18 14:13:54 -0400

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,31 +1,71 @@
-const roastPorkUrl = "http://localhost:3000/dishes/1"
-const dishName = document.querySelector(".dish-details h2")
-const dishImg = document.querySelector(".dish-details img")
-const dishDescription = document.querySelector(".description textarea")
-const customerReview = document.querySelector("ul.reviews")
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
+document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault()
+  const roastPorkUrl = "http://localhost:3000/dishes/1"
+  const dishName = document.querySelector(".dish-details h2")
+  const dishImg = document.querySelector(".dish-details img")
+  const dishDescription = document.querySelector(".description textarea")
+  const customerReview = document.querySelector("ul.reviews")
+  
+  
   fetch(roastPorkUrl)
   .then(res => res.json())
   .then(data => {
     dishName.innerHTML = data.name
     dishImg.src = data.image_url
-    dishDescription.innerHTML = data.description
-    const reviews = data.reviews
-    reviews.forEach(review => {
-      const dishReviews = document.createElement("li") 
-      customerReview.append(dishReviews)
-      dishReviews.innerHTML = reviews
+    dishDescription.innerHTML = data.description 
+    customerReview.innerHTML = ""
+    data.reviews.forEach(review => {
+      const dishListItem = document.createElement("li")    
+      dishListItem.innerHTML = review
+      customerReview.append(dishListItem)
+      })
+   })
 
-    })
+   const updateDishButton = document.querySelector('button')
+   updateDishButton.addEventListener("click", (event) => {
+     event.preventDefault()
+     const newDescription = dishDescription.value
+     
+     const configurationObject = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+          description : newDescription
+      })
+    }
+    fetch("http://localhost:3000/dishes/1", configurationObject)
+    .then(res => res.json())
+    .then(dish => {
+      dishDescription.value
+      })
+  })
+     
+  const leaveReviewButton = document.querySelector('input')
+  leaveReviewButton.addEventListener("click", (event) => {
+    event.preventDefault()
+    const newReviewItem = document.createElement('li')
+    const reviewInput = document.querySelector(".review-form textarea")
     
-    })
-      
-    })
+    newReviewItem.innerHTML = reviewInput.value
+    customerReview.append(newReviewItem)
+    
+
+    
+
+  })
+
+
+
+
+})
+
+
+
+
+
       
 
   
